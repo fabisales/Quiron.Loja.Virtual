1977d844669e8d0068662bee32227d4e1e2b4d8a
fsales <fsales@TI-04>
2016-12-23 11:21:49 -0200

    Novas classes

--- Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -1,10 +1,6 @@
-﻿using Quiron.LojaVirtual.Dominio.Entidade;
-using System;
-using System.Collections.Generic;
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace Quiron.LojaVirtual.Dominio.Repositorio
 {
@@ -12,6 +8,11 @@ namespace Quiron.LojaVirtual.Dominio.Repositorio
     {
 
         public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
 
     }
 }
