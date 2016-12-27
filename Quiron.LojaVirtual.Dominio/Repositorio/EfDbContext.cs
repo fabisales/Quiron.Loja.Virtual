185eeb533ad6831af53c0d5a26700cb00d376e43
fsales <fsales@TI-04>
2016-12-27 10:28:24 -0200

    Entity

--- Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -9,9 +9,10 @@ namespace Quiron.LojaVirtual.Dominio.Repositorio
 
         public DbSet<Produto> Produtos { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) // remover pluralização(o entity framework costuma a colocar as palavras em plural nas tabelas)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos"); // Mostrando ao programa que produto = produtos ----- isso foi feito pois ao removermos a pluralização o programa não conseguiria associar 
         }
 
     }
